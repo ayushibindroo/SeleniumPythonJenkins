bda44de98586a1bd8a58cb4d5ee6ba0709ff1af0
ayushibindroo <56725172+ayushibindroo@users.noreply.github.com>
2023-02-24 18:14:51 +0530

    Add files via upload

---
+++ Ex01_HandleChromeBrowser.py
@@ -0,0 +1,12 @@
+import unittest
+from selenium import webdriver
+import time
+class MyTestCase(unittest.TestCase):
+    def test_launch_browser(self):
+        filePath = "C:\\Users\\Ayushi Bindroo\\PycharmProjects\\SeleniumProject\\drivers\\chromedriver.exe"
+        driver = webdriver.Chrome(filePath)
+        time.sleep(1)
+        driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
